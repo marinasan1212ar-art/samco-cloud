1e033a03424d97d420b4e8bb6b9cc0dc2c8537de
marinasan1212ar-art <marinasan1212ar@gmail.com>
2026-08-16 19:20:03 +0300

    Directly wrap WSGI application with WhiteNoise

--- samco_cloud/wsgi.py
+++ samco_cloud/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for samco_cloud project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.1/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from pathlib import Path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'samco_cloud.settings')
 
 application = get_wsgi_application()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+application = WhiteNoise(application, root=str(BASE_DIR / 'staticfiles'), prefix='static/')
